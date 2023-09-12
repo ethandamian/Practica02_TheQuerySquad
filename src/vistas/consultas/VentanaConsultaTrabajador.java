243396c97fe99314637bab68e50b2cd3371afeb5
MeliLiz <melifernandez@ciencias.unam.mx>
2023-09-12 14:48:18 -0600

    fuentes

--- src/vistas/consultas/VentanaConsultaTrabajador.java
+++ src/vistas/consultas/VentanaConsultaTrabajador.java
@@ -1,5 +1,8 @@
 package vistas.consultas;
 import javax.swing.table.DefaultTableModel;
+
+import vistas.FuenteProyecto;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
@@ -26,6 +29,8 @@ public class VentanaConsultaTrabajador extends VentanaConsultasMenu{
 	protected JLabel lblIncioContrato;
 	protected JLabel lblGeneroValor;
 	protected JLabel lblGenero;
+	private String urlFuenteStringBold = "src/fuentes/RobotoCondensed-Bold.ttf";
+	private String urlFuenteStringPlain = "src/fuentes/RobotoCondensed-Regular.ttf";
 	
 	public VentanaConsultaTrabajador() {
 		lblBusqueda.setText("Buscar por RFC");
@@ -33,115 +38,115 @@ public class VentanaConsultaTrabajador extends VentanaConsultasMenu{
 		lblTitulo.setText("CONSULTAS DE VETERINARIOS");
 		
 		lblRFC = new JLabel("RFC:");
-		lblRFC.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblRFC.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblRFC.setBounds(34, 11, 126, 26);
 		panelSur.add(lblRFC);
 		
 		JLabel lblRFCValor = new JLabel("  ");
-		lblRFCValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblRFCValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblRFCValor.setBounds(34, 37, 126, 26);
 		panelSur.add(lblRFCValor);
 		
 		lblNombreValor = new JLabel("  ");
-		lblNombreValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblNombreValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblNombreValor.setBounds(34, 100, 126, 26);
 		panelSur.add(lblNombreValor);
 		
 		lblNombre = new JLabel("Nombre:");
-		lblNombre.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblNombre.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblNombre.setBounds(34, 74, 126, 26);
 		panelSur.add(lblNombre);
 		
 		lblApellidoMaValor = new JLabel("  ");
-		lblApellidoMaValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblApellidoMaValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblApellidoMaValor.setBounds(34, 164, 126, 26);
 		panelSur.add(lblApellidoMaValor);
 		
 		lblApellidoMaterno = new JLabel("Apellido Materno:");
-		lblApellidoMaterno.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblApellidoMaterno.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblApellidoMaterno.setBounds(34, 137, 126, 26);
 		panelSur.add(lblApellidoMaterno);
 		
 		lblApellidoPaValor = new JLabel("   ");
-		lblApellidoPaValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblApellidoPaValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblApellidoPaValor.setBounds(34, 227, 126, 26);
 		panelSur.add(lblApellidoPaValor);
 		
 		lblApellidoPaterno = new JLabel("Apellido Paterno:");
-		lblApellidoPaterno.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblApellidoPaterno.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblApellidoPaterno.setBounds(34, 201, 126, 26);
 		panelSur.add(lblApellidoPaterno);
 		
 		lblCalleValor = new JLabel("  ");
-		lblCalleValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblCalleValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblCalleValor.setBounds(202, 37, 169, 26);
 		panelSur.add(lblCalleValor);
 		
 		lblCalle = new JLabel("Calle:");
 		lblCalle.setHorizontalAlignment(SwingConstants.LEFT);
-		lblCalle.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblCalle.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblCalle.setBounds(202, 11, 126, 26);
 		panelSur.add(lblCalle);
 		
 		lblNumInteriorValor = new JLabel("   ");
-		lblNumInteriorValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblNumInteriorValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblNumInteriorValor.setBounds(202, 100, 126, 26);
 		panelSur.add(lblNumInteriorValor);
 		
 		lblNumeroInterior = new JLabel("Numero interior:");
-		lblNumeroInterior.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblNumeroInterior.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblNumeroInterior.setBounds(202, 74, 126, 26);
 		panelSur.add(lblNumeroInterior);
 		
 		lblNumeroExteriorValor = new JLabel("   ");
-		lblNumeroExteriorValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblNumeroExteriorValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblNumeroExteriorValor.setBounds(202, 164, 126, 26);
 		panelSur.add(lblNumeroExteriorValor);
 		
 		lblNumeroExterior = new JLabel("Numero Exterior:");
-		lblNumeroExterior.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblNumeroExterior.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblNumeroExterior.setBounds(202, 137, 126, 26);
 		panelSur.add(lblNumeroExterior);
 		
 		lblFechaNacimientoValor = new JLabel("   ");
-		lblFechaNacimientoValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblFechaNacimientoValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblFechaNacimientoValor.setBounds(202, 228, 126, 26);
 		panelSur.add(lblFechaNacimientoValor);
 		
 		lblFechaNacimiento = new JLabel("Fecha de Nacimiento:");
-		lblFechaNacimiento.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblFechaNacimiento.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblFechaNacimiento.setBounds(202, 201, 138, 26);
 		panelSur.add(lblFechaNacimiento);
 		
 		lblEmailValor = new JLabel("    ");
-		lblEmailValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblEmailValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblEmailValor.setBounds(392, 38, 126, 26);
 		panelSur.add(lblEmailValor);
 		
 		lblEmail = new JLabel("Email:");
-		lblEmail.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblEmail.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblEmail.setBounds(392, 11, 126, 26);
 		panelSur.add(lblEmail);
 		
 		lblInicioContratoValor = new JLabel("    ");
-		lblInicioContratoValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblInicioContratoValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblInicioContratoValor.setBounds(392, 101, 126, 26);
 		panelSur.add(lblInicioContratoValor);
 		
 		lblIncioContrato = new JLabel("Inicio de Contrato:");
 		lblIncioContrato.setHorizontalAlignment(SwingConstants.LEFT);
-		lblIncioContrato.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblIncioContrato.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblIncioContrato.setBounds(392, 74, 126, 26);
 		panelSur.add(lblIncioContrato);
 		
 		lblGeneroValor = new JLabel("   ");
-		lblGeneroValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblGeneroValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblGeneroValor.setBounds(392, 164, 126, 26);
 		panelSur.add(lblGeneroValor);
 		
 		lblGenero = new JLabel("Genero:");
 		lblGenero.setHorizontalAlignment(SwingConstants.LEFT);
-		lblGenero.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblGenero.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblGenero.setBounds(392, 137, 126, 26);
 		panelSur.add(lblGenero);
 	}
--- src/vistas/consultas/VentanaConsultarBioma.java
+++ src/vistas/consultas/VentanaConsultarBioma.java
@@ -1,11 +1,17 @@
 package vistas.consultas;
 
 import javax.swing.JPanel;
+
+import vistas.FuenteProyecto;
+
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
 
 public class VentanaConsultarBioma extends VentanaConsultasMenu{
+	
+	private String urlFuenteStringBold = "src/fuentes/RobotoCondensed-Bold.ttf";
+	private String urlFuenteStringPlain = "src/fuentes/RobotoCondensed-Regular.ttf";
 
 	/**
 	 * Create the panel.
@@ -17,62 +23,62 @@ public class VentanaConsultarBioma extends VentanaConsultasMenu{
 		panelSur.setBackground(new Color(166, 187, 141));
 		
 		JLabel lblTipo = new JLabel("Tipo de Bioma:");
-		lblTipo.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblTipo.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblTipo.setBounds(81, 22, 126, 26);
 		panelSur.add(lblTipo);
 		
 		JLabel lblTipoValor = new JLabel("  ");
-		lblTipoValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblTipoValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblTipoValor.setBounds(81, 48, 126, 26);
 		panelSur.add(lblTipoValor);
 		
 		JLabel lblNumeroDeJaulas = new JLabel("Numero de jaulas:");
-		lblNumeroDeJaulas.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblNumeroDeJaulas.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblNumeroDeJaulas.setBounds(81, 95, 126, 26);
 		panelSur.add(lblNumeroDeJaulas);
 		
 		JLabel lblNumeroDeJaulasValor = new JLabel("  ");
-		lblNumeroDeJaulasValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblNumeroDeJaulasValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblNumeroDeJaulasValor.setBounds(81, 121, 126, 26);
 		panelSur.add(lblNumeroDeJaulasValor);
 		
 		JLabel lblNumeroDeCuidadores = new JLabel("Numero de Cuidadores: ");
-		lblNumeroDeCuidadores.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblNumeroDeCuidadores.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblNumeroDeCuidadores.setBounds(81, 182, 145, 26);
 		panelSur.add(lblNumeroDeCuidadores);
 		
 		JLabel lblNumeroDeCuidadoresValor = new JLabel("  ");
-		lblNumeroDeCuidadoresValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblNumeroDeCuidadoresValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblNumeroDeCuidadoresValor.setBounds(81, 208, 126, 26);
 		panelSur.add(lblNumeroDeCuidadoresValor);
 		
 		JLabel lblNumeroDeVeterinarios = new JLabel("Numero de Veterinarios:");
-		lblNumeroDeVeterinarios.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblNumeroDeVeterinarios.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblNumeroDeVeterinarios.setBounds(286, 22, 153, 26);
 		panelSur.add(lblNumeroDeVeterinarios);
 		
 		JLabel lblNumeroDeVeterinariosValor = new JLabel("  ");
-		lblNumeroDeVeterinariosValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblNumeroDeVeterinariosValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblNumeroDeVeterinariosValor.setBounds(286, 48, 126, 26);
 		panelSur.add(lblNumeroDeVeterinariosValor);
 		
 		JLabel lblNumeroDeAnimales = new JLabel("Numero de Animales:");
-		lblNumeroDeAnimales.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblNumeroDeAnimales.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblNumeroDeAnimales.setBounds(286, 95, 153, 26);
 		panelSur.add(lblNumeroDeAnimales);
 		
 		JLabel lblNumeroDeAnimalesValor = new JLabel("  ");
-		lblNumeroDeAnimalesValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblNumeroDeAnimalesValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblNumeroDeAnimalesValor.setBounds(286, 121, 126, 26);
 		panelSur.add(lblNumeroDeAnimalesValor);
 		
 		JLabel lblServicioAVisitantes = new JLabel("Servicio a visitantes:");
-		lblServicioAVisitantes.setFont(new Font("Roboto Condensed", Font.BOLD, 15));
+		lblServicioAVisitantes.setFont(FuenteProyecto.createFont(urlFuenteStringBold, 15));
 		lblServicioAVisitantes.setBounds(286, 182, 153, 26);
 		panelSur.add(lblServicioAVisitantes);
 		
 		JLabel lblServicioAVisitantesValor = new JLabel("  ");
-		lblServicioAVisitantesValor.setFont(new Font("Roboto Condensed", Font.PLAIN, 13));
+		lblServicioAVisitantesValor.setFont(FuenteProyecto.createFont(urlFuenteStringPlain, 13));
 		lblServicioAVisitantesValor.setBounds(286, 208, 126, 26);
 		panelSur.add(lblServicioAVisitantesValor);
 		panelNorte.setBackground(new Color(60, 98, 85));
