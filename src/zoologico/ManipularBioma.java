b761109fa1bda250f59ed6bf9d5cab4c86c69e87
RodrigoSanchez06 <rogerxdgta@ciencias.unam.mx>
2023-09-13 15:57:46 -0600

    add: indicaciones Alan

--- src/zoologico/ManipularBioma.java
+++ src/zoologico/ManipularBioma.java
@@ -1,9 +1,29 @@
 package zoologico;
 
+import Clases.Bioma;
+import Clases.Veterinario;
+
 public class ManipularBioma extends ManipularZoologico {
 
-    public ManipularBioma(String path) {
-        super(path);
-    }
+	String path = "src/archivosCSV/Bioma.csv";
 
+	public Bioma leerBioma(String id) {
+    	String[] values =  this.leer(id); //Lee el veterinario en el csv y regresa un String
+    	
+    	//EL metodo debe regresar un  Objeto Bioma con todos los valores que piden
+    	//Si values devuelve un null entonces no encontró el id y tienes que regresar un null
+    	return null;
+    }
+    
+    public boolean eliminar(String id) {
+    	return this.eliminar(id);
+    }
+    
+    public boolean inserta( String entidad) {
+    	return this.inserta(entidad);
+    }
+    
+    public boolean editar(String entidad, String id) {
+    	return this.editar(entidad, id);
+    }
 }
--- src/zoologico/ManipularVeterinario.java
+++ src/zoologico/ManipularVeterinario.java
@@ -1,10 +1,31 @@
 package zoologico;
 
+import Clases.Veterinario;
+
 public class ManipularVeterinario extends ManipularZoologico {
-    public ManipularVeterinario(String path) {
-        super(path);
-    }
 
     String path = "src/archivosCSV/Veterinario.csv";
-
+    
+    public Veterinario leerVeterinario(String id) {
+    	String[] values =  this.leer(id); //Lee el veterinario en el csv y regresa un String
+    	
+    	//EL metodo debe regresar un  Objeto veterinario con todos los valores que piden
+    	//Si values devuelve un null entonces no encontró el id y tienes que regresar un null
+    	return null;
+    }
+    
+    public boolean eliminar(String id) {
+    	return this.eliminar(id);
+    }
+    
+    public boolean inserta(Veterinario veretrinario) {
+    	
+    	//El numero de telefono y los emails, debes separarlos y pasar toda la entidad en string y un RFC formato "nombre,blabla,blalbal,blalbla,blalba," (checa los archivos csv)
+    	return this.inserta("RFC", "ENTIDADXD");//Valores de ejemplo
+    }
+    
+    public boolean editar(String entidad, String id) {
+    	return this.editar(entidad, id);
+    }
+    
 }
